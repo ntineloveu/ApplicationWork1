4f69e069118dc2e5be1878cc0199afde90892af5
Jittarin <Jittarin8857@gmail.com>
2017-08-09 23:40:16 +0700

    commit2

--- app/src/main/java/com/example/jittarin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jittarin/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.jittarin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textViewHello = (TextView) findViewById(R.id.textViewHello);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String s = "Year";
+        textViewHello.setText(s);
     }
 }
